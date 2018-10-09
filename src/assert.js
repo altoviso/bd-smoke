4e1762d8418dbae39a984c83b7de75a3df3d9ffe
Rawld Gill <rgill@altoviso.com>
2018-10-09 11:24:37 -0700

    rfe: make assert counting public

--- src/assert.js
+++ src/assert.js
@@ -4,16 +4,20 @@ function resetAssertCount(){
 	assertCount = 0;
 }
 
+function bumpAssertCount(){
+	++assertCount;
+}
+
 function getAssertCount(){
 	return assertCount;
 }
 
 function assert(value, message){
 	// trivial assert; any function that throws upon a detected error will work
-	++assertCount;
+	bumpAssertCount();
 	if(!value){
 		throw new Error(message || "fail");
 	}
 }
 
-export {resetAssertCount, getAssertCount, assert};
+export {resetAssertCount, bumpAssertCount, getAssertCount, assert};
