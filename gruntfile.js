4eeabb678e58bcef78936f7fb9063031ced28f82
rcgill <rgill@altoviso.com>
2019-08-19 09:59:57 -0700

    rfe: eslint fixes

--- gruntfile.js
+++ gruntfile.js
@@ -1,34 +1,31 @@
 /* eslint-disable no-console */
+const cp = require('child_process');
 
-function rollup(){
-	let cp = require("child_process");
-	cp.execSync("node node_modules/rollup/bin/rollup -c", {cwd:__dirname}, (error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.log(`stderr: ${stderr}`);
-	});
+function rollup() {
+    cp.execSync('node node_modules/rollup/bin/rollup -c', { cwd: __dirname }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
 }
 
-module.exports = function(grunt){
-	"use strict";
+module.exports = grunt => {
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.registerTask('rollup', rollup);
 
-	grunt.registerTask("rollup", rollup);
-
-	grunt.initConfig({
-		exec: {
-			rollup: "./node_modules/.bin/rollup -c"
-		},
-		watch: {
-			rollup: {
-				files: ["./src/**/*.*"],
-				tasks: ["rollup"]
-			}
-		}
-	});
-
+    grunt.initConfig({
+        exec: {
+            rollup: './node_modules/.bin/rollup -c'
+        },
+        watch: {
+            rollup: {
+                files: ['./src/**/*.*'],
+                tasks: ['rollup']
+            }
+        }
+    });
 };
